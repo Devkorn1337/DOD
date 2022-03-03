73de824bd73e200ea623b5cd5a7a56ab04b4fa61
DEV KORN <99119935+Devkorn1337@users.noreply.github.com>
2022-03-03 16:44:16 +0700

    Add files via upload

---
+++ DoD.py
@@ -0,0 +1,66 @@
+print('''
+██████╗░░█████╗░██████╗░
+██╔══██╗██╔══██╗██╔══██╗
+██║░░██║██║░░██║██║░░██║
+██║░░██║██║░░██║██║░░██║
+██████╔╝╚█████╔╝██████╔╝
+╚═════╝░░╚════╝░╚═════╝░
+''')
+print("s : ")
+s = int(input(''))
+print("v : ")
+v = int(input(''))
+t = s//v
+print("T")
+print(t)
+if t == 20 :{
+    print("20s to hit")
+}
+if t == 19 :{
+    print("19s to hit")
+}
+if t == 18 :{
+    print("18s to hit")
+}
+if t == 17 :{
+    print("17s to hit")
+}
+if t == 16 :{
+    print("16s to hit")
+}
+if t == 15 :{
+    print("15s to hit")
+}
+if t == 14 :{
+    print("14s to hit")
+}
+if t == 13 :{
+    print("13s to hit")
+}
+if t == 12 :{
+    print("12s to hit")
+}
+if t == 11 :{
+    print("11s to hit")
+}
+if t == 10 :{
+    print("10s to hit")
+}
+if t == 9 :{
+    print("9s to hit")
+}
+if t == 8 :{
+    print("8s to hit")
+}
+if t == 7 :{
+    print("7s to hit")
+}
+if t == 6 :{
+    print("6s to hit")
+}
+if t == 5 :{
+    print("impact distance")
+}
+if t < 5 :{
+    print("impact distance")
+}
